5906c6f498a0c3c1632b7bc7e99a45f1911d8bdb
Dan <160802240+mixsz@users.noreply.github.com>
2024-10-10 04:38:19 +0000

    utilizando ponteiro

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
   char resposta[10], opcao[10];
   char confirma_senha[15];
   int id_usuario, menu = 1, oi = 1;
-
+  int *NV; // ponteiro para o indice numero de usuarios (da variavel contador_cadastros)
 
   FILE *ler = fopen("usuarios.txt", "r"); // LER ARQUIVO TXT
   char linha[2550]; // VARIAVEL QUE ARMAZENA TEMPORARIAMENTE OS CADASTROS DO TXT
@@ -58,83 +58,85 @@ int main() {
     printf( "%s\n", usuarios[0].senha);  
     fclose(ler);
 
-      FILE *escreve1 = fopen("usuarios.txt", "a"); // APLICA UM \N INICIAL CASO NÃO TENHA NADA NO ARQUIVO
-      if (contador_cadastros == 0){
-      fprintf(escreve1, "\n");}
-      fclose(escreve1);  
-
-      int NV = contador_cadastros; // Novo Cadastro
-
-  while (sair != 1){
-    printf("%d\n",NV);
-
-    printf("1 - Criar conta \n");
-    printf("2 - Acessar conta\n");
-    printf("3 - Sair\n");
-
-    if (bemvindo == 0){
-      printf("\nBem-vindo! Digite a opção desejada: ");
+    FILE *escreve1 = fopen("usuarios.txt", "a"); // APLICA UM \N INICIAL CASO NÃO TENHA NADA NO ARQUIVO
+    if (contador_cadastros == 0){
+      fprintf(escreve1, "\n");
     }
-    else{
-      printf("\nDigite a opção desejada: ");
-    }
-    fgets(resposta, sizeof(resposta), stdin);
+    fclose(escreve1);  
 
-    if (resposta[0] != '1' && resposta[0] != '2' && resposta[0] != '3' || strlen(resposta) > 2){
-      puts("Resposta inválida!\n");
-      bemvindo = 1;
-    }
-    if (resposta[0] == '1'){ // Criar conta
-      bemvindo = 1;
-
-      if (NV >= 15){
-        puts("Número máximo de contas atingido!\n");
+    NV = &contador_cadastros; // Novo Cadastro
+    while (sair != 1){
+      printf("contador %d\n",contador_cadastros);
+      printf("ponteiro %d\n",*NV);
+  
+  
+      printf("1 - Criar conta \n");
+      printf("2 - Acessar conta\n");
+      printf("3 - Sair\n");
+  
+      if (bemvindo == 0){
+        printf("\nBem-vindo! Digite a opção desejada: ");
       }
       else{
-        while (1){//  CADASTRO DE USERNAME      
-            printf("\nDigite seu username (máximo 16 carácteres): ");
-            fgets(usuarios[NV].username, sizeof(usuarios[NV].username), stdin);
-            usuarios[NV].username[strcspn(usuarios[NV].username, "\n")] = '\0';
-
-            if (verifica_username(usuarios[NV].username, NV, usuarios) == 0) { // se for = 0 -> username valido
-              puts("Username cadastrado!\n");
-              break;
-            }
-          }
-
-          while (1){ // CADASTRO DE SENHA
-            printf("Digite sua senha (máximo 14 caractéres): ");
-            fgets(usuarios[NV].senha, sizeof(usuarios[NV].senha), stdin);  
-
-            if (verifica_senha(usuarios[NV].senha) == 0) { // se for = 0 -> senha valida
-              puts("Senha cadastrada!\n");
-              usuarios[NV].ficha = 10; // inicia a conta com 10 fichas
-              usuarios[NV].pontuacao1 = 0; // inicia a conta com 0 pontos
-              break;
-            }
-          }       
-          if(confirma_cadastro(NV, usuarios) == 1){ // confirma o cadastro e escreve no txt
-            NV++; // adiciona um novo cadastro
-            contador_cadastros++;
-          }
+        printf("\nDigite a opção desejada: ");
       }
-    }
-
-    if (resposta[0] == '2'){
-      id_usuario = -1; // o id sera iniciado como -1 pois n existira o indice -1 no vetor
-      permissao_acesso = login(&bemvindo, &id_usuario, NV, usuarios); // se for 1 significa que o user logou!
-      if (permissao_acesso == 1){
-        while (menu == 1){
-          puts("\n1. Sla");
-          puts("2. Slaaaa");
-          puts("3. Slaaewf");
-          puts("4. Slweferg");
-          puts("");
-           while (1){
-            if (oi == 1) { // dar oi na primeira vez da mensagem
+      fgets(resposta, sizeof(resposta), stdin);
+  
+      if (resposta[0] != '1' && resposta[0] != '2' && resposta[0] != '3' || strlen(resposta) > 2){
+        puts("Resposta inválida!\n");
+        bemvindo = 1;
+      }
+      if (resposta[0] == '1'){ // Criar conta
+        bemvindo = 1;
+  
+        if (*NV >= 15){
+          puts("Número máximo de contas atingido!\n");
+        }
+        else{
+          while (1){//  CADASTRO DE USERNAME      
+              printf("\nDigite seu username (máximo 16 carácteres): ");
+              fgets(usuarios[*NV].username, sizeof(usuarios[*NV].username), stdin);
+              usuarios[*NV].username[strcspn(usuarios[*NV].username, "\n")] = '\0';
+  
+              if (verifica_username(usuarios[*NV].username, *NV, usuarios) == 0) { // se for = 0 -> username valido
+                puts("Username cadastrado!\n");
+                break;
+              }
+            }
+  
+            while (1){ // CADASTRO DE SENHA
+              printf("Digite sua senha (máximo 14 caractéres): ");
+              fgets(usuarios[*NV].senha, sizeof(usuarios[*NV].senha), stdin);  
+  
+              if (verifica_senha(usuarios[*NV].senha) == 0) { // se for = 0 -> senha valida
+                puts("Senha cadastrada!\n");
+                usuarios[*NV].ficha = 10; // inicia a conta com 10 fichas
+                usuarios[*NV].pontuacao1 = 0; // inicia a conta com 0 pontos
+                break;
+              }
+            }       
+            if(confirma_cadastro(*NV, usuarios) == 1){ // confirma o cadastro e escreve no txt
+              contador_cadastros++;
+            }
+        }
+      }
+  
+      if (resposta[0] == '2'){
+        id_usuario = -1; // o id sera iniciado como -1 pois n existira o indice -1 no vetor
+        permissao_acesso = login(&bemvindo, &id_usuario, *NV, usuarios); // se for 1 significa que o user logou!
+        if (permissao_acesso == 1){
+          while (menu == 1){
+            puts("\n1. Sla");
+            puts("2. Slaaaa");
+            puts("3. Slaaewf");
+            puts("4. Slweferg");
+            puts("");
+            while (1){
+              if (oi == 1) { // dar oi na primeira vez da mensagem
               printf("Olá %s! Digite a opção desejada: ",usuarios[id_usuario].username);
               oi = 0;
-            } else {
+            } 
+            else{
               printf("Digite a opção desejada: ");
             }
             fgets(opcao,sizeof(opcao),stdin);
@@ -164,17 +166,17 @@ int main() {
             
           }
           if (opcao[0] == '4'){
-            
+              
             puts("\nTESTA");
             deseja_continuar(usuarios[id_usuario].username, &menu, &sair);
-            
+              
           }
         } // fim do loop menu
       } // fim do permissao = 1
-
-
+  
+  
     } // fim resposta = 2    
-
+  
     if (resposta[0] == '3'){ // sai do programa
       puts("\nTenha um ótimo dia!\n");
       sair = 1;
